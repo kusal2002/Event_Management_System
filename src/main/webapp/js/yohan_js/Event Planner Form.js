2dec1be8119f2b084e9264e8c3d1bf78c0d55d08
yohan317 <yohanravindu40@gmail.com>
2024-10-08 23:23:44 +0530

    yoo

---
+++ src/main/webapp/js/yohan_js/Event Planner Form.js
@@ -0,0 +1,4 @@
+document.querySelector(".save-btn").addEventListener("click", function(event) {
+    event.preventDefault();
+    alert("Form submitted!");
+});
---
+++ src/main/webapp/js/yohan_js/event planner update.js
@@ -0,0 +1,4 @@
+document.querySelector(".save-btn").addEventListener("click", function(event) {
+    event.preventDefault();
+    alert("Profile has been edited and saved successfully!");
+});
